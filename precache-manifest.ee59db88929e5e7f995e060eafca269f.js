93491a7a20bb2cc5a0d7b3a160c7d01eebaae50f
Kalyan553 <57609028+Kalyan553@users.noreply.github.com>
2020-09-25 14:51:18 +0530

    Updates

---
+++ precache-manifest.ee59db88929e5e7f995e060eafca269f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c2c7388eff4525272361e0e3da0530",
+    "url": "/reactdeployment/index.html"
+  },
+  {
+    "revision": "4dc48cc2c3348d41870c",
+    "url": "/reactdeployment/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4548857618d83e536b1b",
+    "url": "/reactdeployment/static/js/2.c364232a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactdeployment/static/js/2.c364232a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dc48cc2c3348d41870c",
+    "url": "/reactdeployment/static/js/main.70322c5d.chunk.js"
+  },
+  {
+    "revision": "2c6c756dec42a22192fd",
+    "url": "/reactdeployment/static/js/runtime-main.9aea53c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactdeployment/static/media/logo.5d5d9eef.svg"
+  }
+]);
